62f959a956aa349dff15acdd9f8dff5ca29e5c08
Sravya <gudursravya@gmail.com>
2023-07-10 11:54:00 -0500

    Added a animated wrapper for everty section

--- src/cointainer/Skills/Skills.jsx
+++ src/cointainer/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from "react";
 import {motion} from 'framer-motion';
-import { AppWrap } from "../../wrapper";
+import { AppWrap,MotionWrap } from "../../wrapper";
 import { urlFor,client } from "../../client";
 import {Tooltip as ReactTooltip} from 'react-tooltip';
 import "react-tooltip/dist/react-tooltip.css";
@@ -100,4 +100,4 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills,'skills')
+export default AppWrap(MotionWrap(Skills,'app__skills'),'skills','app__whitebg')
